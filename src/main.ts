d68b9ad3fd697be4169aed5f5906e072981d1819
Sean Linton <sean.linton@veeva.com>
2024-10-09 16:12:06 -0700

    add todo app template

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,54 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "../amplify/data/resource";
+import './style.css';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+Amplify.configure(outputs);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+const client = generateClient<Schema>();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const todos: Array<Schema["Todo"]["type"]> = [];
+    const todoList = document.getElementById("todoList") as HTMLUListElement;
+    const addTodoButton = document.getElementById("addTodo") as HTMLButtonElement;
+
+    addTodoButton.addEventListener("click", createTodo);
+
+    function updateUI() {
+        todoList.innerHTML = '';
+        todos.forEach(todo => {
+            const li = document.createElement('li');
+            li.textContent = todo.content ?? '';
+            todoList.appendChild(li);
+        });
+    }
+
+    function createTodo() {
+      console.log('createTodo');
+      const content = window.prompt("Todo content");
+      if (content) {
+          client.models.Todo.create({ content }).then(response => {
+              if (response.data && !response.errors) {
+                  todos.push(response.data);
+                  updateUI();
+              } else {
+                  console.error('Error creating todo:', response.errors);
+                  alert('Failed to create todo.');
+              }
+          }).catch(error => {
+              console.error('Network or other error:', error);
+              alert('Failed to create todo due to a network or other error.');
+          });
+      }
+  }
+  
+
+    client.models.Todo.observeQuery().subscribe({
+        next: (data) => {
+            todos.splice(0, todos.length, ...data.items);
+            updateUI();
+        }
+    });
+});
