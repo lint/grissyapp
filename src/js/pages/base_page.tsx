d0f4981dc89ceb910f71ddfeaf99d1d115bb79e2
Sean Linton <47455468+lint@users.noreply.github.com>
2024-10-18 00:15:12 -0700

    Update base_page.tsx

--- src/js/pages/base_page.tsx
+++ src/js/pages/base_page.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Navbar from '../components/navbar/navbar';
-import './page.css';
+import './pages.css';
 
 export interface BasePageProps {
     page_content: React.ReactNode;
