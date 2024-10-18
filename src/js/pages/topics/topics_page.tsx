ffab3953c000b61a875916ea2085168aadd2e132
Sean Linton <47455468+lint@users.noreply.github.com>
2024-10-18 00:18:13 -0700

    Update topics_page.tsx

--- src/js/pages/topics/topics_page.tsx
+++ src/js/pages/topics/topics_page.tsx
@@ -1,9 +1,9 @@
 
 import React from 'react';
-import BasePage from '../../components/layout/base_page';
 import TopicDashboard from '../../components/topics/dashboard';
 import TopicDashboardRightToolbarItems from '../../components/topics/right_toolbar_items';
 import ToolbarNavItems from '../../components/navbar/toolbar_nav_items';
+import BasePage from '../base_page';
 
 export default function TopicsPage() {
 
